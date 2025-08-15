814440345cd1ecd62840770b6fa70945cb5be0da
Mateusz Jankowski <mateuszjankowski@Mateuszs-MBP.lan>
2025-08-15 14:59:03 +0200

    Add env handling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.scss';
+
 import { App } from './app';
+
+import './index.scss';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
