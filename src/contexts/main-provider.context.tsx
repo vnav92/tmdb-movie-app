582aa0f8c4d61d1220feb24644ddfbebd2128de0
Mateusz Jankowski <mateuszjankowski@Mateuszs-MBP.lan>
2025-08-15 14:50:26 +0200

    Initial setup

---
+++ src/contexts/main-provider.context.tsx
@@ -0,0 +1,23 @@
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { IntlProvider } from 'react-intl';
+
+import messages from '../intl/en_US.json';
+import { BrowserRouter } from 'react-router-dom';
+
+const DEFAULT_LOCALE = 'en';
+
+type MainProviderProps = {
+  children: React.ReactNode;
+};
+
+const queryClient = new QueryClient();
+
+export const MainProvider: React.FC<MainProviderProps> = ({ children }) => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <IntlProvider locale={DEFAULT_LOCALE} messages={messages}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </IntlProvider>
+    </QueryClientProvider>
+  );
+};
